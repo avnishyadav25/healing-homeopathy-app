3476d62c78b8f007d4517fd1f670069784c7b5d0
Avnish Yadav <avnishyadav2561995@gmail.com>
2024-11-05 15:13:37 +0530

    changes for coming soon

---
+++ web-app/src/pages/public/discussion/DiscussionPage.js
@@ -0,0 +1,14 @@
+import ComingSoon from '../../../components/ComingSoon';
+import Template from '../../../components/common/Template';
+
+
+const DiscussionPage = () => {
+  return (
+    <Template>
+  <ComingSoon />
+  </Template>
+
+  );
+};
+
+export default DiscussionPage;
--- web-app/src/pages/public/services/OrderMedicinePage.js
+++ web-app/src/pages/public/services/OrderMedicinePage.js
@@ -2,21 +2,15 @@
 
 import React from 'react';
 import Template from '../../../components/common/Template';
+import ComingSoon from '../../../components/ComingSoon';
+
 
 
 const OrderMedicinePage = () => {
   return (
     <div className="order-medicine-container">
       <Template>
-      <br /><br /><br /><br /><br />
-      <h1>Order Homeopathic Medicines</h1>
-      <p>
-        Get your prescribed homeopathic medicines delivered to your doorstep. 
-        Choose from our wide range of homeopathic remedies tailored to your needs.
-      </p>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      {/* Add a form or product list here */}
+      <ComingSoon />
       </Template>
     </div>
   );
